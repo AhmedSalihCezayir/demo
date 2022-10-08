17c3299561504cedb93e33502ec498dd31163418
Ahmed Salih Cezayir <asalihcezayir@gmail.com>
2022-10-08 13:44:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+studentA = Student("Ayse", 18, 11111)
+studentB = Student("Mert", 20, 22222)
+
+studentA.say_name()
+studentA.say_age()
+studentA.info_dump()
+
+studentB.say_name()
+studentB.say_age()
+studentB.info_dump()
+studentB.info_dump_different()
