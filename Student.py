6bd3ac237daa889aca43aff06b4957f8cf4f6f71
Ahmed Salih Cezayir <asalihcezayir@gmail.com>
2022-10-08 13:42:05 +0300

    Create Student.py

---
+++ Student.py
@@ -0,0 +1,12 @@
+class Student():
+  def __init__(self, name, age, id):
+    super.__init__(name, age)
+    self.id = id
+    
+  def say_name(self):
+    print("My name is: ", self.name, " and my id is: ", self.id)
+    
+  def info_dump_different(self):
+    self.say_name()
+    self.say_age()
+    print("Thats all")
